21da70b208e35393a18cce4dc2b910d36348bdfd
Ashish Kamble <ashishkamble191191@gmail.com>
2020-05-05 10:53:20 +0530

    Updates

---
+++ precache-manifest.0f9be9ca259100f8057b1c3a5da5834c.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40122a3954a434c885924ee71c8754dd",
+    "url": "/AshITech/index.html"
+  },
+  {
+    "revision": "35e2ec48f751468e1f4b",
+    "url": "/AshITech/static/css/2.66f801d2.chunk.css"
+  },
+  {
+    "revision": "d9976ef51f84eb60f953",
+    "url": "/AshITech/static/css/main.58521172.chunk.css"
+  },
+  {
+    "revision": "35e2ec48f751468e1f4b",
+    "url": "/AshITech/static/js/2.3f57754c.chunk.js"
+  },
+  {
+    "revision": "d9976ef51f84eb60f953",
+    "url": "/AshITech/static/js/main.a97b72ec.chunk.js"
+  },
+  {
+    "revision": "be648eb826a9cc224c44",
+    "url": "/AshITech/static/js/runtime~main.b3a006ee.js"
+  },
+  {
+    "revision": "39b2c3031be6b4ea96e2e3e95d307814",
+    "url": "/AshITech/static/media/Roboto-Bold.39b2c303.woff2"
+  },
+  {
+    "revision": "dc81817def276b4f21395f7ea5e88dcd",
+    "url": "/AshITech/static/media/Roboto-Bold.dc81817d.woff"
+  },
+  {
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "/AshITech/static/media/Roboto-Bold.e31fcf18.ttf"
+  },
+  {
+    "revision": "ecdd509cadbf1ea78b8d2e31ec52328c",
+    "url": "/AshITech/static/media/Roboto-Bold.ecdd509c.eot"
+  },
+  {
+    "revision": "3b813c2ae0d04909a33a18d792912ee7",
+    "url": "/AshITech/static/media/Roboto-Light.3b813c2a.woff"
+  },
+  {
+    "revision": "46e48ce0628835f68a7369d0254e4283",
+    "url": "/AshITech/static/media/Roboto-Light.46e48ce0.ttf"
+  },
+  {
+    "revision": "69f8a0617ac472f78e45841323a3df9e",
+    "url": "/AshITech/static/media/Roboto-Light.69f8a061.woff2"
+  },
+  {
+    "revision": "a990f611f2305dc12965f186c2ef2690",
+    "url": "/AshITech/static/media/Roboto-Light.a990f611.eot"
+  },
+  {
+    "revision": "4d9f3f9e5195e7b074bb63ba4ce42208",
+    "url": "/AshITech/static/media/Roboto-Medium.4d9f3f9e.eot"
+  },
+  {
+    "revision": "574fd0b50367f886d359e8264938fc37",
+    "url": "/AshITech/static/media/Roboto-Medium.574fd0b5.woff2"
+  },
+  {
+    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
+    "url": "/AshITech/static/media/Roboto-Medium.894a2ede.ttf"
+  },
+  {
+    "revision": "fc78759e93a6cac50458610e3d9d63a0",
+    "url": "/AshITech/static/media/Roboto-Medium.fc78759e.woff"
+  },
+  {
+    "revision": "2751ee43015f9884c3642f103b7f70c9",
+    "url": "/AshITech/static/media/Roboto-Regular.2751ee43.woff2"
+  },
+  {
+    "revision": "30799efa5bf74129468ad4e257551dc3",
+    "url": "/AshITech/static/media/Roboto-Regular.30799efa.eot"
+  },
+  {
+    "revision": "ba3dcd8903e3d0af5de7792777f8ae0d",
+    "url": "/AshITech/static/media/Roboto-Regular.ba3dcd89.woff"
+  },
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/AshITech/static/media/Roboto-Regular.df7b648c.ttf"
+  },
+  {
+    "revision": "7500519de3d82e33d1587f8042e2afcb",
+    "url": "/AshITech/static/media/Roboto-Thin.7500519d.woff"
+  },
+  {
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "/AshITech/static/media/Roboto-Thin.94998475.ttf"
+  },
+  {
+    "revision": "954bbdeb86483e4ffea00c4591530ece",
+    "url": "/AshITech/static/media/Roboto-Thin.954bbdeb.woff2"
+  },
+  {
+    "revision": "dfe56a876d0282555d1e2458e278060f",
+    "url": "/AshITech/static/media/Roboto-Thin.dfe56a87.eot"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/AshITech/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/AshITech/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/AshITech/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/AshITech/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/AshITech/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/AshITech/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/AshITech/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
